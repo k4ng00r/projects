8ad846b136c1b21455e357c671c2784c2034ce07
Kangoor Brown <kangoorbrown@wp.pl>
2025-10-12 23:04:38 +0200

    2025-10-12: P0 - fix data

--- P0_Random-Life-Advice-Generator/script.js
+++ P0_Random-Life-Advice-Generator/script.js
@@ -1,31 +1,43 @@
-const advices = [
-  'Nie odpisuj na maile po północy 😴',
-  'Bootcamp raz w życiu, drama na zawsze 😂',
-  'AI może i sprytnie podpowiada, ale Twój kod – Twój honor 💪',
-  'Kawa to najlepszy debugger ☕',
-  'Nie martw się, nawet junior ma prawo do błędów 🤷‍♂️'
+const jsonFiles = [
+  'https://k4ng00r.github.io/projects/P0_Random-Life-Advice-Generator/data/jesus.json',
+  'https://k4ng00r.github.io/projects/P0_Random-Life-Advice-Generator/data/budda.json',
+  'https://k4ng00r.github.io/projects/P0_Random-Life-Advice-Generator/data/confucius.json',
+  'https://k4ng00r.github.io/projects/P0_Random-Life-Advice-Generator/data/socrates.json'
 ];
 
 const colors = ['#C1440E','#E4D3B3','#5A7359','#2A9DF4','#FF7F32', '#8B8C84', '#A0522D'];
 const btn = document.querySelector('#advice-btn');
 const display = document.querySelector('#advice-display');
 
-btn.addEventListener('click', () => {
-  const randomAdvice = advices[Math.floor(Math.random() * advices.length)];
-  const randomColor = colors[Math.floor(Math.random() * colors.length)];
-  document.body.style.backgroundColor = randomColor;
+btn.addEventListener('click', async () => {
+  try {
+    // 1️⃣ draw JSON file
+    const randomFile = jsonFiles[Math.floor(Math.random() * jsonFiles.length)];
 
-  // Fade-out
-  display.style.transition = 'opacity 0.25s, transform 0.25s';
-  display.style.opacity = '0';
-  display.style.transform = 'translateY(10px)';
+    // 2️⃣ fetch data
+    const response = await fetch(randomFile);
+    const advices = await response.json();
 
-  setTimeout(() => {
-    // change text
-    display.textContent = randomAdvice;
+    // 3️⃣ draw advice
+    const randomAdvice = advices[Math.floor(Math.random() * advices.length)];
 
-    // Fade-in
-    display.style.opacity = '1';
-    display.style.transform = 'translateY(0)';
-  }, 250);
+    // 4️⃣ draw bgc
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    document.body.style.backgroundColor = randomColor;
+
+    // 5️⃣ animation fade-out + fade-in
+    display.style.transition = 'opacity 0.25s, transform 0.25s';
+    display.style.opacity = '0';
+    display.style.transform = 'translateY(10px)';
+
+    setTimeout(() => {
+      display.textContent = randomAdvice;
+      display.style.opacity = '1';
+      display.style.transform = 'translateY(0)';
+    }, 250);
+
+  } catch (err) {
+    console.error('Błąd ładowania porady:', err);
+    display.textContent = 'Nie udało się pobrać porady 😞';
+  }
 });
