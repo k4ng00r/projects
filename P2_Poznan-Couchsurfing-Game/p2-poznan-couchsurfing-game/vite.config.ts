09ec1f295e1ac0bd5b5a71bf4cb84de5c57bcc45
Kangoor Brown <kangoorbrown@wp.pl>
2025-10-23 19:37:12 +0200

    2025-10-23: refactor -> install preact, add vite config file

---
+++ P2_Poznan-Couchsurfing-Game/p2-poznan-couchsurfing-game/vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+import preact from '@preact/preset-vite';
+
+export default defineConfig({
+  plugins: [preact()],
+  // USTAW bazę pod GirHub Pages (nazwa repo jako podkatalog)
+  base: '/p2-poznan-couchsurfing-game/',
+});
